cf04b570fb834c362d26cb7756ba99ef7eac60a6
mytkadev <mytkalol@gmail.com>
2024-09-17 17:48:46 -0300

    1

--- src/pages/landingPage-en.tsx
+++ src/pages/landingPage-en.tsx
@@ -1,6 +1,5 @@
 import NavbarEN from '../components/EN/navbar-en'
 import HeroSectionEN from '../components/EN/heroSection-en'
-import Teams from '../components/teams'
 
 export function LandingPageEN() {
     return (
